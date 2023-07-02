5b913c8aebfe31a3356c3ea829d8fa196179744e
owenliang <owenliang1990@gmail.com>
2023-07-02 20:19:47 +0800

    1

--- unet.py
+++ unet.py
@@ -12,10 +12,12 @@ class UNet(nn.Module):
 
         channels=[img_channel]+channels
 
+        # 每个encoder block增加1倍channel，减少1倍尺寸
         self.enc_blocks=nn.ModuleList()
         for i in range(len(channels)-1):
             self.enc_blocks.append(EncoderBlock(channels[i],channels[i+1]))
         
+        # 每个decoder block减少1倍channel，增加1倍尺寸
         self.dec_blocks=nn.ModuleList()
         for i in range(len(channels)-1):
             self.dec_blocks.append(DecoderBlock(channels[-i-1]*2,channels[-i-2])) # 有残差结构,所以channal输入翻倍
@@ -28,10 +30,9 @@ class UNet(nn.Module):
             residual.append(x)
         for dec_block in self.dec_blocks:
             residual_x=residual.pop(-1)
-            x=dec_block(torch.cat((residual_x,x),dim=1))
-        return x 
+            x=dec_block(torch.cat((residual_x,x),dim=1))    # 残差用于纵深channel维
+        return x
         
-
 if __name__=='__main__':
     batch_x=torch.stack((train_dataset[0][0],train_dataset[1][0]),dim=0).to(DEVICE) # 2个图片拼batch, (2,1,96,96)
     batch_x=batch_x*2-1 # 像素值调整到[-1,1]之间,以便与高斯噪音值范围匹配
